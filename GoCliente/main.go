670623ea66ce6d5a930e09b2a854c98f950d4209
Julio Peñaloza <julioalejandro.7734@gmail.com>
2022-11-19 15:13:57 -0500

    PostGetDelete cliente

--- GoCliente/main.go
+++ GoCliente/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -25,12 +27,84 @@ func obtenerProductoID(idProd string, canalProductos chan *http.Response) {
 func obtenerTiendaID(idTienda string, canalTiendas chan *http.Response) {
 	//Instanciar un cliente HTTP
 	cliente := http.Client{}
-	resp, err := cliente.Get("http://127.0.0.1:2323/estudiantesID?idTienda=" + idTienda)
+	resp, err := cliente.Get("http://127.0.0.1:8000/tienda/" + idTienda)
 	if err != nil {
 		log.Fatal("Error conmiendo servicio tiendas: ", idTienda)
 		os.Exit(1)
 	}
 	canalTiendas <- resp
+}
+func PostTienda(tienda model.Tienda, canalTiendas chan *http.Response) {
+	//Instanciar un cliente HTTP
+	cliente := http.Client{}
+	jsonTienda, err := json.Marshal(tienda)
+	if err != nil {
+		// Maneja el error de acuerdo a tu situación
+		log.Fatalf("Error codificando usuario como JSON: %v", err)
+	}
+	url := "http://127.0.0.1:8000/tienda"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonTienda))
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ")
+		os.Exit(1)
+	}
+	resp, err := cliente.Do(req)
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ")
+		os.Exit(1)
+	}
+	canalTiendas <- resp
+}
+func PostProducto(producto model.Producto, canalProductos chan *http.Response) {
+	//Instanciar un cliente HTTP
+	cliente := http.Client{}
+	jsonProducto, err := json.Marshal(producto)
+	if err != nil {
+		// Maneja el error de acuerdo a tu situación
+		log.Fatalf("Error codificando usuario como JSON: %v", err)
+	}
+	url := "http://127.0.0.1:8000/tienda"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonProducto))
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ")
+		os.Exit(1)
+	}
+	resp, err := cliente.Do(req)
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ")
+		os.Exit(1)
+	}
+	canalProductos <- resp
+}
+func deleteTiendaID(idTienda string, canalRespuestas chan *http.Response) {
+	cliente := http.Client{}
+	url := "http://127.0.0.1:8000/tienda/" + idTienda
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ", idTienda)
+		os.Exit(1)
+	}
+	resp, err := cliente.Do(req)
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ", idTienda)
+		os.Exit(1)
+	}
+	canalRespuestas <- resp
+}
+func deleteProductoID(idProd string, canalRespuestas chan *http.Response) {
+	cliente := http.Client{}
+	url := "http://127.0.0.1:8000/tienda/" + idProd
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ", idProd)
+		os.Exit(1)
+	}
+	resp, err := cliente.Do(req)
+	if err != nil {
+		log.Fatal("Error conmiendo servicio tiendas: ", idProd)
+		os.Exit(1)
+	}
+	canalRespuestas <- resp
 }
 
 func main() {
@@ -41,7 +115,7 @@ func main() {
 
 	var producto model.Producto
 	var tienda model.Tienda
-	fmt.Print("Menú de opciones: \n 1: Get por id \n Cosas \n Su opción: ")
+	fmt.Print("Menú de opciones: \n 1: Get por id \n 2: Delete por id \n 3: Post \n Su opción: ")
 	fmt.Scan(&op)
 	switch op {
 	case 1:
@@ -52,7 +126,7 @@ func main() {
 		fmt.Scan(&idprod)
 		fmt.Print("Ingrese el ID de la tienda: ")
 		fmt.Scan(&idtienda)
-		go obtenerProductoID(idprod, canalProductos)
+		//go obtenerProductoID(idprod, canalProductos)
 		go obtenerTiendaID(idtienda, canalTiendas)
 		var datosProducto, datosTienda *http.Response
 		//Vamos a realizar dos lecturas de los canales
@@ -73,5 +147,114 @@ func main() {
 		fmt.Println("Producto consultado: ", producto)
 		fmt.Println("Tienda consultada: ", tienda)
 		fmt.Println("-------------------------------")
+	case 2:
+		var idtienda string
+		var idprod string
+		fmt.Print("Ingrese el ID del producto: ")
+		fmt.Scan(&idprod)
+		fmt.Print("Ingrese el ID de la tienda: ")
+		fmt.Scan(&idtienda)
+		var resProducto, resTienda *http.Response
+		go deleteTiendaID(idtienda, canalTiendas)
+		go deleteProductoID(idprod, canalProductos)
+		for i := 0; i < 2; i++ {
+			select {
+			case respProductos := <-canalProductos:
+				resProducto = respProductos
+			case respTienda := <-canalTiendas:
+				resTienda = respTienda
+			}
+		}
+		// Read Response Body
+		respBody, err := ioutil.ReadAll(resTienda.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Display Results
+		fmt.Println("response Status : ", resTienda.Status)
+		fmt.Println("response Headers : ", resTienda.Header)
+		fmt.Println("response Body : ", string(respBody))
+		respBody2, err := ioutil.ReadAll(resProducto.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Display Results
+		fmt.Println("response Status : ", resProducto.Status)
+		fmt.Println("response Headers : ", resProducto.Header)
+		fmt.Println("response Body : ", string(respBody2))
+	case 3:
+		var codigoTienda string
+		var nombreTienda string
+		var telefonoTienda int
+		var ciudadTienda string
+		var direccionTienda string
+		var descripcionTienda string
+		fmt.Print("Ingrese el ID de la tienda: ")
+		fmt.Scan(&codigoTienda)
+		fmt.Print("Ingrese el nombre de la tienda: ")
+		fmt.Scan(&nombreTienda)
+		fmt.Print("Ingrese el telefono de la tienda: ")
+		fmt.Scan(&telefonoTienda)
+		fmt.Print("Ingrese la ciudad de la tienda: ")
+		fmt.Scan(&ciudadTienda)
+		fmt.Print("Ingrese la direccion de la tienda: ")
+		fmt.Scan(&direccionTienda)
+		fmt.Print("Ingrese la descripcion de la tienda: ")
+		fmt.Scan(&descripcionTienda)
+		tiendaStr := model.Tienda{codigoTienda, nombreTienda, telefonoTienda, ciudadTienda, direccionTienda, descripcionTienda}
+		var codigoProducto int
+		var nombreProducto string
+		var precioProducto int
+		var descripcionProducto string
+		var tiendaProducto string
+		fmt.Print("Ingrese el ID del producto: ")
+		fmt.Scan(&codigoProducto)
+		fmt.Print("Ingrese el nombre del producto: ")
+		fmt.Scan(&nombreProducto)
+		fmt.Print("Ingrese el precio del producto: ")
+		fmt.Scan(&precioProducto)
+		fmt.Print("Ingrese la descripcion del producto: ")
+		fmt.Scan(&descripcionProducto)
+		fmt.Print("Ingrese la tienda del producto: ")
+		fmt.Scan(&tiendaProducto)
+		prodStr := model.Producto{codigoProducto, nombreProducto, precioProducto, descripcionProducto, tiendaProducto}
+		var resProducto, resTienda *http.Response
+		go PostTienda(tiendaStr, canalTiendas)
+		go PostProducto(prodStr, canalProductos)
+		for i := 0; i < 2; i++ {
+			select {
+			case respProductos := <-canalProductos:
+				resProducto = respProductos
+			case respTienda := <-canalTiendas:
+				resTienda = respTienda
+			}
+		}
+		// Read Response Body
+		respBody, err := ioutil.ReadAll(resTienda.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Display Results
+		fmt.Println("response Status : ", resTienda.Status)
+		fmt.Println("response Headers : ", resTienda.Header)
+		fmt.Println("response Body : ", string(respBody))
+		respBody2, err := ioutil.ReadAll(resProducto.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Display Results
+		fmt.Println("response Status : ", resProducto.Status)
+		fmt.Println("response Headers : ", resProducto.Header)
+		fmt.Println("response Body : ", string(respBody2))
+	default:
+		fmt.Println("Opción inválida")
 	}
 }
--- GoCliente/model/producto.go
+++ GoCliente/model/producto.go
@@ -1,9 +1,9 @@
 package model
 
 type Producto struct {
-	codigo      int    `json:"codigo,omitempty"`
-	nombre      string `json:"nombre,omitempty"`
-	precio      int    `json:"precio,omitempty"`
-	descripcion string `json:"descripcion,omitempty"`
-	tienda      string `json:"tienda,omitempty"`
+	Codigo      int    `json:"codigo,omitempty"`
+	Nombre      string `json:"nombre,omitempty"`
+	Precio      int    `json:"precio,omitempty"`
+	Descripcion string `json:"descripcion,omitempty"`
+	Tienda      string `json:"tienda,omitempty"`
 }
